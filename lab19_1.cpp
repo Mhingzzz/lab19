4e330f19e59e8497e304c7fee1e458a5b3fe06b5
mm <nathaphong_p@cmu.ac.th>
2022-01-25 00:36:10 +0700

    Finished

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
+#include<cstring>
 
 using namespace std;
 
@@ -20,22 +21,80 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string file, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    string s;
+    char n[200];
+    int a,b,c,sum;
+    fstream myfile;
+    myfile.open(file);
 
+    while(getline(myfile,s)){
+        string g = s;
+        char *text = new char[g.size()+1];
+        char form[] = "%[^:]: %d %d %d";
+        strcpy(text,g.c_str());
+        sscanf(text, form, n,&a, &b, &c);
+        sum = a + b + c;
+        names.push_back(n);
+        scores.push_back(sum);
+        grades.push_back(score2grade(sum));
+
+    }   
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command :";
+    cin >> command;
+    if (toUpperStr(command) == "NAME" ||toUpperStr(command) == "GRADE" )
+    {
+        cin.ignore();
+        getline(cin, key);
+    }
+    
+    
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    bool condi = false;
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < names.size(); i++)
+    {
+        if (toUpperStr(key) == toUpperStr(names.at(i)))
+        {
+            condi = true;
+            cout << names.at(i) <<"'s " << scores.at(i) <<endl;
+            cout << names.at(i) <<"'s " << grades.at(i) <<endl;
+        }
+        
+    }
+    if (condi == false)
+    {
+        cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
+    
 }
 
-void searchGrade(){
+void searchGrade(vector<string> names, vector<int> scores, vector<char> grades, string key){
 
+    bool condi = false;
+    vector<char> k(key.begin(), key.end());
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < names.size(); i++)
+    {
+        if (k.at(0) == grades.at(i))
+        {
+            condi = true;
+            cout << names.at(i) << " " <<"("<<scores.at(i)<<")"<<endl;
+        }
+        
+    }
+    if (condi == false)
+    {
+        cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
-
 
 int main(){
     string filename = "name_score.txt";
